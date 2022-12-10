65ae682002b01aa6e3b2e646257a45e13c61ec2b
FURB\dtlsilva <daniel_thls@yahoo.com.br>
2022-12-09 21:55:24 -0300

    Aula 09-12-2022 Como criar DLL e introdução ao FMX

---
+++ Fire Monkey/Aula FMX/Aula 1 - To do list/USplash.pas
@@ -0,0 +1,42 @@
+unit USplash;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Layouts;
+
+type
+  TForm2 = class(TForm)
+    Layout1: TLayout;
+    Image1: TImage;
+    Timer1: TTimer;
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.fmx}
+
+uses UfrmPrincipal;
+
+procedure TForm2.Timer1Timer(Sender: TObject);
+begin
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.MainForm := frmPrincipal;
+  frmPrincipal.Show;
+
+  Timer1.Enabled := false;
+
+  Self.Close;
+end;
+
+end.
